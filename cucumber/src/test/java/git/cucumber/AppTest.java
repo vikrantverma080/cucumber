d5dfc57afea116d7da7f2bf0e1b01a6c26743551
vikrant verma <vikrantverma080@gmail.com>
2022-05-01 07:45:38 +0530

    Updated AppTest.java

---
+++ cucumber/src/test/java/git/cucumber/AppTest.java
@@ -0,0 +1,21 @@
+package git.cucumber;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+        System.out.println("ttttt");
+    }
+}
